62156df84f1c9f0ec46fab2c5576266f3ee260c2
TenesacaJ <mewtwo9594@gmail.com>
2024-05-06 06:00:05 -0500

    Tercer

--- src/app/tabinicio/tabinicio.page.ts
+++ src/app/tabinicio/tabinicio.page.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabinicioPage implements OnInit {
 
-  constructor() { }
+  showMessage: boolean = false;
+  messageIndex: number = 0;
+  messages: string[] = [
+    "Bienvedo!",
+    "¡Tu satisfacción es nuestro mayor objetivo!",
+    "Hola, ¿cómo va el día?"
+  ];
+  message: string = this.messages[this.messageIndex];
+
+  constructor() {}
 
   ngOnInit() {
+    setInterval(() => {
+      this.showMessage = true;
+      this.messageIndex = (this.messageIndex + 1) % this.messages.length;
+      this.message = this.messages[this.messageIndex];
+      setTimeout(() => {
+        this.showMessage = false;
+      }, 3000); // Cambia el mensaje cada 10 segundos
+    }, 5000);
   }
 
 }
